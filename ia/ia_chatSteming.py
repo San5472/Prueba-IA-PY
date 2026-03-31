904f263da1040e79fbaa2c1dd9388eb4c1aa1e07
sanzambra <santiagozambrano751@gmail.com>
2026-03-31 15:16:43 -0500

    Arreglo orden

--- ia/ia_chatSteming.py
+++ ia/ia_chatSteming.py
@@ -15,31 +15,6 @@ messages=[
 
 print("Chat iniciado con la ia, Escribe 'salir' para terminar.\n")
 
-
-chat_stream = cliente.chat.completions.create(
-    messages=messages,
-    model="llama-3.3-70b-versatile",
-    temperature=0.5,
-    max_completion_tokens=1024,
-    stream=True
-)
-
-print("IA: ", end="")
-respuesta_completa = ""
-
-for chunk in chat_stream:
-    contenido = chunk.choices[0].delta.content
-    if contenido:
-        print(contenido, end="", flush=True)
-        respuesta_completa += contenido
-
-print() 
-
-messages.append({
-    "role": "assistant",
-    "content": respuesta_completa
-})
-
 while True: 
     user_input = input("Tu: ")
 
@@ -47,7 +22,28 @@ while True:
         print("Chat Cerrado")
         break
             
-        messages.append({
-            "role": "user",
-            "content": user_input
-        })
+    messages.append({
+        "role": "user",
+        "content": user_input
+    })
+
+
+    chat_stream = cliente.chat.completions.create(
+        messages=messages,
+        model="llama-3.3-70b-versatile",
+        temperature=0.5,
+        max_completion_tokens=1024,
+        stream=True
+    )
+
+    print("IA: ", end="")
+    respuesta_completa = ""
+
+    for chunk in chat_stream:
+        contenido = chunk.choices[0].delta.content
+        if contenido:
+            print(contenido, end="", flush=True)
+            respuesta_completa += contenido
+
+    print() 
+
